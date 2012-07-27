93682e62f5718807d97d6e49ffca399cceeaeb56
Jaakko Juutila <jaakko.juutila@gmail.com>
2012-07-27 22:53:13 +0300

    Make be_accessible matcher's messages more descriptive

--- spec/support/be_accessible_matcher.rb
+++ spec/support/be_accessible_matcher.rb
@@ -4,7 +4,7 @@ RSpec::Matchers.define :be_accessible do |attribute|
   match do |response|
     response.class.accessible_attributes.include?(attribute)
   end
-  description { "be accessible :#{attribute}" }
-  failure_message_for_should { ":#{attribute} should be accessible" }
-  failure_message_for_should_not { ":#{attribute} should not be accessible" }
+  description { "be accessible to mass-assignment: #{attribute}" }
+  failure_message_for_should { ":#{attribute} should be accessible to mass-assignment" }
+  failure_message_for_should_not { ":#{attribute} should not be accessible to mass-assignment" }
 end
