e832250d9ae4dc816a36e562db92a8fdd3661315
Jaakko Juutila <jaakko.juutila@gmail.com>
2012-07-27 21:00:12 +0300

    Apply configurations from AM application

--- config/application.rb
+++ config/application.rb
@@ -29,17 +29,17 @@ module Signing
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = "Helsinki"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :fi
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :B02K_CUSTID]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
