cf263768a249df2a0b4b8bfbcb5faa2a631afc57
Igor Afonov <afonov@gmail.com>
2012-09-09 14:00:29 +0300

    Supress errors if secret tokens file is empty or not present.

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,12 @@
 require File.expand_path('../application', __FILE__)
 
 # preload various tokens to local ENV
-api_tokens = YAML.load(File.read(Rails.root.join('config', 'api_keys', "#{Rails.env}.yml"))) rescue {}
-api_tokens.each{ |key, val| ENV[key] = val }
+begin
+  api_tokens = YAML.load(File.read(Rails.root.join('config', 'api_keys', "#{Rails.env}.yml"))) rescue {}
+  api_tokens.each{ |key, val| ENV[key] = val }
+rescue
+  puts "No secret keys found" # We don't have logger yet, just puts it to console
+end
 
 # Initialize the rails application
 Signing::Application.initialize!
