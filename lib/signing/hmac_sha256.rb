b6da31cdc3a5488041f256c07a4711c1750508b7
Jaakko Juutila <jaakko.juutila@gmail.com>
2012-09-25 19:29:58 +0300

    Convert message to a query string if it's not a string already

--- lib/signing/hmac_sha256.rb
+++ lib/signing/hmac_sha256.rb
@@ -3,7 +3,8 @@
 module Signing
   module HmacSha256
     def sign key, message
-      OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, key, message)
+      message = message.to_param unless message.is_a? String
+      OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, key, message).upcase
     end
 
     def sign_array key, array
