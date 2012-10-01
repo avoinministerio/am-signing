dcd820e298f4467f60d6212227dccef692468803
aleksi <aleksi.rossi@iki.fi>
2012-10-01 03:07:55 +0300

    Fixed Sampo returning via :post

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Signing::Application.routes.draw do
 #  match "/signatures"                             => "signatures#begin_authenticating", via: :post, as: :signature_begin_authenticating
   match "/signatures"                             => "signatures#begin_authenticating", via: :get, as: :signature_begin_authenticating
   match "/signatures/:id/finalize_signing"        => "signatures#finalize_signing",     via: :put
-  match "/signatures/:id/returning/:servicename"  => "signatures#returning",            via: :get
-  match "/signatures/:id/cancelling/:servicename" => "signatures#cancelling",           via: :get
-  match "/signatures/:id/rejecting/:servicename"  => "signatures#rejecting",            via: :get
+  match "/signatures/:id/returning/:servicename"  => "signatures#returning"             # originally via: :get but Sampo returns with :post
+  match "/signatures/:id/cancelling/:servicename" => "signatures#cancelling"
+  match "/signatures/:id/rejecting/:servicename"  => "signatures#rejecting"
 
   match "/heartbeat"                              => "heartbeats#index",                via: :get
 end
